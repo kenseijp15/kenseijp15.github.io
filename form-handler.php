e33e05e34422152d38427eab7c96f86b850f286f
kenseijp15 <kenseijp15@gmail.com>
2020-11-22 10:00:57 -0700

    commit:(

---
+++ form-handler.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['email'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'kenseijp15@outlook.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "kenseijp15@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headerz);
+
+    header("Location: index.html");
+?> 
