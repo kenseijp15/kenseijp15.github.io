9149218cc4e6aa3b86c432f32997f0d64411ba4f
kenseijp15 <kenseijp15@gmail.com>
2020-12-21 10:53:24 -0700

    php-didntwork

--- script.js
+++ script.js
@@ -11,4 +11,39 @@ if (
     }
   });
   observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
+}
+
+
+const hamburger = document.querySelector(".menu-btn");
+hamburger.addEventListener("click", toggleBtn, false);
+
+const navBar = document.querySelector(".fixed-nav");
+
+const flyoutMenu = document.querySelector(".slide-menu");
+flyoutMenu.addEventListener("click", hideMenu, false);
+
+var menuVisible = false;
+  
+function toggleBtn() {
+  menuVisible = !menuVisible
+  if (menuVisible) {
+    flyoutMenu.classList.add("show");
+    navBar.classList.add("menu-visible");
+
+    document.body.style.overflow = "hidden";
+    } else {
+    hideMenu();
   }
+}
+  
+function hideMenu() {
+    flyoutMenu.classList.remove("show");
+    navBar.classList.remove("menu-visible");
+    event.stopPropagation();
+
+    document.body.style.overflow = "auto";
+} 
+
+// for (i = 0; i < 2; i++) {
+//     menuBtn[i].addEventListener("click", hideMenu, false);
+// }
